558b66bcac3b1d3fb9a6330f5254564103cbb75d
unstoppable <internalminds01@gmail.com>
2024-09-01 02:43:51 +0800

    Define the request struct to represent HTTP request

---
+++ rust-framework/own-framework-foundation/src/main.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
+use std::thread;
+
+type HandlerFunction = fn(&Request) -> Response;
---
+++ rust-framework/own-framework-foundation/target/debug/deps/own_framework_foundation-3d3a691e6058552f.d
@@ -0,0 +1,5 @@
+/home/i/full-stack-ui/rust-framework/own-framework-foundation/target/debug/deps/libown_framework_foundation-3d3a691e6058552f.rmeta: src/main.rs
+
+/home/i/full-stack-ui/rust-framework/own-framework-foundation/target/debug/deps/own_framework_foundation-3d3a691e6058552f.d: src/main.rs
+
+src/main.rs:
