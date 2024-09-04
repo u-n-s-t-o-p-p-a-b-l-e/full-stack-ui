540692233a88fe601af554137c70f46c163d552d
unstoppable <internalminds01@gmail.com>
2024-09-05 03:32:36 +0800

    Setting index.html as landing page

--- rust-framework/axum_wallet_app/src/main.rs
+++ rust-framework/axum_wallet_app/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    response::{Html, IntoResponse},
+    Router,
+};
+use std::fs;
+
+use std::net::SocketAddr;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index_page))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+pub async fn index_page() -> impl IntoResponse {
+    let html_content = fs::read_to_string("src/index.html")
+        .expect("Failed to read HTML file");
+    Html(html_content)
 }
